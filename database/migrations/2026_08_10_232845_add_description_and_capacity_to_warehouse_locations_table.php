232384ddd14bd741a52c4a547cdfbbc74d830d5d
SKPnation <ayomideseaz@gmail.com>
2026-08-10 18:33:23 -0500

    added description capapcity if they don't exist

---
+++ database/migrations/2026_08_10_232845_add_description_and_capacity_to_warehouse_locations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('warehouse_locations', function (Blueprint $table) {
+            if (!Schema::hasColumn('warehouse_locations', 'description')) {
+                $table->text('description')->nullable();
+            }
+
+            if (!Schema::hasColumn('warehouse_locations', 'capacity')) {
+                $table->decimal('capacity', 15, 3)->nullable();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('warehouse_locations', function (Blueprint $table) {
+             if (Schema::hasColumn('warehouse_locations', 'description')) {
+                $table->dropColumn('description');
+            }
+
+            if (Schema::hasColumn('warehouse_locations', 'capacity')) {
+                $table->dropColumn('capacity');
+            }
+        });
+    }
+};
